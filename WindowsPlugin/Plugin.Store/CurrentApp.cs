5201c693e5ed7caee1cad85d61ac76e7d4e041fb
Foxman13 <valknut13@gmail.com>
2014-03-31 22:38:05 -0500

    Added initial code
    The beginnings of the project structure

---
+++ WindowsPlugin/Plugin.Store/CurrentApp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#if NETFX_CORE || WINDOWS_PHONE
+using Windows.ApplicationModel.Store;
+#endif
+
+namespace Windows.Plugins.Store
+{
+    public class CurrentApp
+    {
+        public CurrentApp()
+        { }
+
+        public Guid AppId
+        {
+            get
+            {
+#if NETFX_CORE || WINDOWS_PHONE
+                return Windows.ApplicationModel.Store.CurrentApp.AppId;
+#else
+                throw new NotImplementedException();
+#endif
+            }
+        }
+
+        public Uri LinkUri
+        {
+            get { 
+#if NETFX_CORE || WINDOWS_PHONE
+                return Windows.ApplicationModel.Store.CurrentApp.LinkUri; 
+#else
+                throw new NotImplementedException();
+#endif
+            }
+        }
+    }
+}
---
+++ WindowsPlugin/Plugin.Store/LicenseInformation.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#if NETFX_CORE || WINDOWS_PHONE
+using Windows.ApplicationModel.Store;
+#endif
+
+namespace Windows.Plugins.Store
+{
+    public class LicenseInformation
+    {
+        public delegate void LicenseChangedEventHandler();
+        public event LicenseChangedEventHandler LicenseChanged;
+
+        public LicenseInformation()
+        {
+#if NETFX_CORE || WINDOWS_PHONE
+                //return Windows.ApplicationModel.
+#endif
+        }
+
+
+        public DateTimeOffset ExpirationDate { 
+            get 
+            { 
+#if NETFX_CORE || WINDOWS_PHONE
+                return Windows.ApplicationModel.Store.CurrentApp.LicenseInformation.ExpirationDate;
+#else
+                throw new NotImplementedException();
+#endif
+            }
+        }
+
+        public bool IsActive { 
+            get 
+            {
+#if NETFX_CORE || WINDOWS_PHONE
+                return Windows.ApplicationModel.Store.CurrentApp.LicenseInformation.IsActive;
+#else
+                throw new NotImplementedException();
+#endif
+            } 
+        }
+
+
+        public bool IsTrial { 
+            get 
+            {
+#if NETFX_CORE || WINDOWS_PHONE
+                return Windows.ApplicationModel.Store.CurrentApp.LicenseInformation.IsTrial;
+#else
+                throw new NotImplementedException();
+#endif
+            } 
+        }
+
+
+        public Dictionary<String, ProductLicense> ProductLicenses { 
+            get 
+            {
+#if NETFX_CORE || WINDOWS_PHONE
+                
+                //return Windows.ApplicationModel.Store.CurrentApp.LicenseInformation.ProductLicenses.ToDictionary<String, ProductLicense>();
+                return null;
+#else
+                throw new NotImplementedException();
+#endif
+            } 
+        }
+    }
+}
